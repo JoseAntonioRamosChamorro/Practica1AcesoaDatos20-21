834c8504fe08997715abc6047ef6bf90f4d62f8a
Tirano <Tirano@DESKTOP-ABDT0NO>
2020-10-27 17:36:17 +0100

    1 Commit

---
+++ Aceso a Datos tema1/src/ListaDeLaCompra.java
@@ -0,0 +1,92 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+
+public class ListaDeLaCompra implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	String NombreLista;
+	ArrayList<ArticuloAComprar>ListaCompra = new ArrayList<ArticuloAComprar>();
+
+	public ListaDeLaCompra() {
+		NombreLista ="";
+
+	}
+	public ListaDeLaCompra(String nombreLista) {
+		NombreLista = nombreLista;
+	}
+	public String getNombreLista() {
+		return NombreLista;
+
+	}
+	public void setNombreLista(String nombreLista) {
+		NombreLista = nombreLista;
+	}
+	/*
+	  */
+	public void agregarProductoAComprar(ArticuloAComprar articulo) {
+		ListaCompra.add(articulo);
+
+	}
+	/*
+	  */
+	public void eliminarArticulo (String descripcionArticulo){
+		for (int i = 0; i < ListaCompra.size(); i++) {
+			eliminarIgual(descripcionArticulo,ListaCompra.get(i));
+		}
+	}
+	
+	private void eliminarIgual(String descripcionArticulo, ArticuloAComprar articuloAComprar) {
+		if (articuloAComprar.getDescripcion().equals(descripcionArticulo)) {
+			ListaCompra.remove(articuloAComprar);
+		}
+
+	}
+	
+	 
+	public ArrayList<ArticuloAComprar> getArticulos(){
+		return ListaCompra;
+	}
+	public void almacenarListaCompra() {
+		try {
+		File archivoAlmacenar = new File(".\\"+NombreLista+".dat");
+		FileOutputStream fos = new FileOutputStream(archivoAlmacenar);
+		ObjectOutputStream salida = new ObjectOutputStream(fos);	
+		
+		salida.writeObject(this);
+		salida.close();
+		fos.close();
+		}catch (Exception e) {
+			e.getStackTrace();
+			e.getMessage();
+			System.out.println("Error de escritura");
+		}
+		
+	}
+	
+	public static ListaDeLaCompra recuperarListaCompra(String archivo) {
+		ListaDeLaCompra listaRecuperada = new ListaDeLaCompra(archivo);
+		try {
+			File archivoRecuperar = new File(".\\"+archivo+".dat");
+			FileInputStream fis = new FileInputStream(archivoRecuperar);
+			ObjectInputStream entrada = new ObjectInputStream(fis);	
+
+			listaRecuperada = (ListaDeLaCompra) entrada.readObject();
+			System.out.println("Recuperado "+archivo);
+			entrada.close();
+			fis.close();
+			}catch (Exception e) {
+				e.getStackTrace();
+				e.getMessage();
+				System.out.println("Error de recuperación");
+			}
+		return listaRecuperada;
+	 }
+
+}
---
+++ Aceso a Datos tema1/src/Main.java
@@ -0,0 +1,46 @@
+
+public class Main {
+	public static void main(String[] args) {
+		
+		/*-- crear articulos--*/
+		ArticuloAComprar artPlatano = new ArticuloAComprar("Platano",3,"Unidad");
+		ArticuloAComprar artHuevoL = new ArticuloAComprar("Huevo L",1,"L");
+		ArticuloAComprar artPechuga = new ArticuloAComprar("Pollo",1,"KG");
+		ArticuloAComprar artHelado = new ArticuloAComprar("Helado",1,"Paquete");
+		
+		/*--Lista--*/
+		ListaDeLaCompra lista1 = new ListaDeLaCompra("Mercadona");
+		ListaDeLaCompra lista2 = new ListaDeLaCompra("Lidl");
+		
+		
+		/*--Agregar a Lista--*/
+		lista1.agregarProductoAComprar(artPlatano);
+		lista1.agregarProductoAComprar(artHuevoL);
+		lista1.agregarProductoAComprar(artPechuga);
+		
+		lista2.agregarProductoAComprar(artHelado);
+		
+		
+		/*Articulos de las listas */
+
+		System.out.println("Los articulos de la lista "+ lista1.getNombreLista()+" son:");
+		for (ArticuloAComprar articulo : lista1.getArticulos()) {
+
+			System.out.println(articulo.getDescripcion()+" - "+articulo.getCantidad()+" - "+articulo.getUnidad());
+		
+		}
+		System.out.println("Los articulos de la lista "+ lista2.getNombreLista()+" son:");
+		
+		for (ArticuloAComprar articulo : lista2.getArticulos()) {
+
+			System.out.println(articulo.getDescripcion()+" - "+articulo.getCantidad()+" - "+articulo.getUnidad());
+		
+		}
+		System.out.println();
+		lista1.almacenarListaCompra();
+		ListaDeLaCompra.recuperarListaCompra("Mercadona");
+
+		lista2.almacenarListaCompra();
+		ListaDeLaCompra.recuperarListaCompra("Lidl");
+	}
+}
